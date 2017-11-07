fb9487d0cd2d0debb140be55fcb00383ad77c094
David Dashifen Kees <dashifen@dashifen.com>
2017-11-07 09:53:03 -0500

    added PostTypeTrait and TaxonomyTrait

--- src/Controller/ControllerException.php
+++ src/Controller/ControllerException.php
@@ -13,4 +13,6 @@ class ControllerException extends Exception {
 	public const UNKNOWN_STATUS = 2;
 	public const MISSING_METHOD = 3;
 	public const UNKNOWN_SETTING = 4;
+	public const UNKNOWN_TYPE = 5;
+	public const UNKNOWN_TAX = 6;
 }
